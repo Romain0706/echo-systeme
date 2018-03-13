e5a1ae1afbb6ca8abd13a1d8d22eaee357f2816f
Romain Boniface <bonifaceromain59190@gmail.com>
2018-03-13 17:30:40 +0100

    no message

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,6 +1,6 @@
 export const AUTH_CONFIG = {
   clientId: 'Me5FP8FE20RtsbqPNJ26NKQqLdgaxXoX',
   domain: 'echo-systeme.eu.auth0.com',
-  callbackUrl: 'http://localhost:8080/callback',
+  callbackUrl: 'https://romain0706.github.io/callback',
   apiUrl: 'https://echo-systeme.eu.auth0.com/api/v2/'
 }
